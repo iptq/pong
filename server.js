d4f386b41d71a689eb30188d5eb1d72c01d82ec9
Michael Zhang <failed.down@gmail.com>
2015-09-12 06:05:46 -0400

    [backend] user connecting

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+var port = process.env.PORT || 80;
+
 var express = require("express");
 var app = express();
 
 app.use(express.static("web"));
 
-var server = app.listen(process.env.PORT || 80);
+var server = app.listen(port);
+
+var turnIndex = 0;
 
 var Gun = require("gun");
 var gun = Gun({
-	file: "data.json"
+	file: "trash/" + (new Date()).getTime()
 });
 gun.attach(server);
+server.listen(port);
--- web/js/pong.js
+++ web/js/pong.js
@@ -1,3 +1,78 @@
+var username = null;
+while(username == null || username.length < 1 || username == "_") 
+	username = prompt("What's your username?");
+
+var isMyTurn = false;
+
 var Gun = Gun || { };
 var gun = Gun(location.origin + "/gun");
 
+var pong = gun.get("pong").set();
+var users = pong.path("users").set();
+var obj = {}; obj[username] = { score: 0 };
+users.put(obj);
+var existing_users = [ ];
+users.map(function(_user, _username) {
+	var el;
+	el = document.getElementById("u_" + _username);
+	if (el == null) {
+		existing_users.push(_username);
+		existing_users.sort();
+		el = document.createElement("li");
+		el.id = "u_" + _username;
+		document.getElementById("users").appendChild(el);
+	}
+	el.innerHTML = _username + ": " + _user["score"];
+});
+
+/*
+(function(){
+	var array = ['a', 'b', 'c', 'd'];
+	
+	var obj = {1: 'a', 2: 'b', 3: 'c'};
+	
+	var pong = gun.get('pong/data');
+	
+	pong.put({
+		users: {
+			'asdf': true,
+			'fads': true,
+			'dafs': false
+		}
+	});
+	
+	
+	pong.path('users').key('players/pong').map(function(inGame, ID){
+		if(inGame){
+			// idemopotently add or update them.	
+		} else {
+			remove them;	
+		}
+		
+	});
+	
+	
+});
+
+(function(){
+		var pong = gun.get('pong/data');
+	
+		var players = gun.get('pong/room1/players');
+});
+*/
+
+/*
+pong 
+|- users
+   |- player1
+   |- player2
+*/
+
+(function(){
+	var pong = {};
+	pong.users = {};
+	var users = pong.users;
+	
+	users.forEach()
+	pong.users.forEach()
+});
